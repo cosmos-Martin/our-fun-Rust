3e0fb06f66855b8f4d69c7de312900067d1fd802
jokerrobert920 <jokerrobert920@gmail.com>
2022-05-04 14:12:42 +0300

    i fall in love with Rust language

---
+++ hello/target/debug/deps/hello-67a61f26f26e0079.d
@@ -0,0 +1,5 @@
+/home/dev/Documents/ogc/hello/target/debug/deps/hello-67a61f26f26e0079: src/main.rs
+
+/home/dev/Documents/ogc/hello/target/debug/deps/hello-67a61f26f26e0079.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/dev/Documents/ogc/hello/target/debug/hello: /home/dev/Documents/ogc/hello/src/main.rs
